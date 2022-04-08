515ac6a0410dee6e3e87830a64bd2cfb522ad008
Rick Chen <62127656+cycyucheng1010@users.noreply.github.com>
2022-04-08 16:50:40 +0800

    Create UVA10041_Vito'sfamily.c

---
+++ CPE/UVA10041_Vito'sfamily.c
@@ -0,0 +1,29 @@
+```
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int> num;
+int main(void){
+	int n,r,s;
+	
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>r;
+		num.clear();
+		for(int j=0;j<r;j++){
+			cin>>s;
+			num.push_back(s);
+		}
+		sort(num.begin(),num.end());
+		int mid=num[r/2];
+		int sum =0;
+		for(int k=0;k<r;k++){
+			sum =sum + abs(num[k]-mid);
+		}
+		cout<<sum<<endl;
+	}
+
+	return 0;
+}
+```
